bc795af66f020ed625e07e60a8d2de92bd3a930d
tslai <tl@quartile.co>
2020-12-16 09:50:55 +0800

    [ADD] sale_order_reference, gitignore

---
+++ sale_order_reference/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Reference",
+    "version": "14.0.1.0.0",
+    "category": "Sale",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co/",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_order_reference/models/sale_order.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+#   Char, Float, Integer, Text
+#   Many2one, Many2many, One2many
+    order_reference = fields.Char(
+        string="Order Reference",
+#       required=True,
+#       readonly=True,
+#       track_visibility='always',
+#       compute='_compute_order_reference'
+    )
+    
+    def _compute_order_reference(self):
+        for order in self:
+            order.order_reference = order.partner_id.ref
