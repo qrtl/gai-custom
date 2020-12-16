4eab2ff3e1643c7bfcf969a7254d971746e82ae6
TimLai125 <tl@quartile.co>
2020-12-16 03:06:35 +0000

    [UPD] sale_order_reference

--- sale_order_reference/models/sale_order.py
+++ sale_order_reference/models/sale_order.py
@@ -1,7 +1,8 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -13,9 +14,23 @@ class SaleOrder(models.Model):
         string="Custom Reference",
 #       required=True,
 #       readonly=True,
-#       track_visibility='always',
-#       compute='_compute_order_reference'
+        track_visibility='always',
+#                         onchange
+#         compute='_compute_order_reference'
     )
+    
+    @api.constrains('order_reference')
+    def _reference_constrains(self):
+        for rec in self:
+            if rec.order_reference == 'hello':
+                raise ValidationError('Error')
+                
+#     def write(self, vals) 更新
+#     vals = {
+#        'order_reference': 'test'
+#     }
+#     def create(self, vals) 作成
+#     def unlink(self, vals) 削除
     
 #     def _compute_order_reference(self):
 #         for order in self:
