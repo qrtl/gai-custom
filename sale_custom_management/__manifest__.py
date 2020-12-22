377111b044ecd15e0968325a4db90088921797db
TimLai125 <tl@quartile.co>
2020-12-22 14:27:19 +0000

    [ADD] sale_custom_management

---
+++ sale_custom_management/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Custom Management",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co/",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+        "views/sale_menu_views.xml",
+    ],
+    "installable": True,
+}
